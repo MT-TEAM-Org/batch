f5c21f49673265b456052d917c0c7d3a5077c392
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-01-28 17:40:12 +0900

    feat: 초기환경세팅

---
+++ src/main/java/com/playhive/batch/global/config/WebDriverConfig.java
@@ -0,0 +1,19 @@
+package com.playhive.batch.global.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebDriverConfig {
+
+	@Bean
+	public WebDriver webDriver() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // 헤드리스 모드
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver"); // chromedriver 경로 설정
+		return new ChromeDriver(options);
+	}
+}
