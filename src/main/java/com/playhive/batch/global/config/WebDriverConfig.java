dc824c82b631dd82248d2fe1fca0226ebb1ab041
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-09 02:34:42 +0900

    Update WebDriverConfig.java

--- src/main/java/com/playhive/batch/global/config/WebDriverConfig.java
+++ src/main/java/com/playhive/batch/global/config/WebDriverConfig.java
@@ -19,16 +19,16 @@ public class WebDriverConfig {
 		options.addArguments("--ignore-ssl-errors=yes");
 		options.addArguments("--ignore-certificate-errors");
 		// 운영체제에 따른 ChromeDriver 경로 설정
-		String os = System.getProperty("os.name").toLowerCase();
-		String chromeDriverPath;
-
-		if (os.contains("win") || os.contains("mac")) {
-			chromeDriverPath = "./driver/chromedriver"; // Windows 경로
-		} else { // Linux 포함
-			chromeDriverPath = "/play-hive-batch/chromedriver"; // Linux 경로
-		}
-
-		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		// String os = System.getProperty("os.name").toLowerCase();
+		// String chromeDriverPath;
+		//
+		// if (os.contains("win") || os.contains("mac")) {
+		// 	chromeDriverPath = "./driver/chromedriver"; // Windows 경로
+		// } else { // Linux 포함
+		// 	chromeDriverPath = "/play-hive-batch/chromedriver"; // Linux 경로
+		// }
+		//
+		// System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		return new ChromeDriver(options);
 	}
 }
