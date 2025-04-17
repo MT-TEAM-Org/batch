4820692e0dbf87ccbddddbfe61cf7b6cdb87a163
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-04-18 05:06:56 +0900

    fix: 기사날짜로 변경

--- src/main/java/com/playhive/batch/news/repository/NewsRepository.java
+++ src/main/java/com/playhive/batch/news/repository/NewsRepository.java
@@ -9,7 +9,7 @@ import com.playhive.batch.news.entity.NewsCategory;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
 
-	@Query("SELECT n.source FROM p_news n WHERE n.category = :category ORDER BY n.createDate DESC LIMIT 1")
+	@Query("SELECT n.source FROM p_news n WHERE n.category = :category ORDER BY n.postDate DESC LIMIT 1")
 	String findRecentPostDate(@Param("category") NewsCategory category);
 
 }
