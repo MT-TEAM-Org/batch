aeaf228427063e0de6be0f2221f1cc97786e6eda
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-04-17 21:47:28 +0900

    feat: 2시간 마다 배치돌도록 수정

--- src/main/java/com/playhive/batch/schedule/Scheduler.java
+++ src/main/java/com/playhive/batch/schedule/Scheduler.java
@@ -25,7 +25,7 @@ public class Scheduler {
 	private final Job gameCrawlJob;
 	private final JobLauncher jobLauncher;
 
-	@Scheduled(cron = "0 0 5 * * *") // 매일 오전 5시 0분 0초에 실행
+	@Scheduled(cron = "0 0 */2 * * *") // 매일 2시간 마다 실행
 	public void newsCrawlJob() throws
 		JobInstanceAlreadyCompleteException,
 		JobExecutionAlreadyRunningException,
