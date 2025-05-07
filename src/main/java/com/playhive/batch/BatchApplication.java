abc4ebbcc6fe3e7b1193d66fc6fc69cb09bc8fdb
송지영 <thdwldud8@gmail.com>
2025-05-08 00:47:57 +0900

    fix: baseTime null로 저장되는 부분 수정

--- src/main/java/com/playhive/batch/BatchApplication.java
+++ src/main/java/com/playhive/batch/BatchApplication.java
@@ -2,14 +2,16 @@ package com.playhive.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class BatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
 
 }
