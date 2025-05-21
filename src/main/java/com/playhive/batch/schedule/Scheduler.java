e744ff900b716870ec7a57b0aecedce5fc2dca9d
송지영 <thdwldud8@gmail.com>
2025-05-21 16:29:45 +0900

    chore: 게임 크롤링은 batch없이 돌아가도록 수정

--- src/main/java/com/playhive/batch/schedule/Scheduler.java
+++ src/main/java/com/playhive/batch/schedule/Scheduler.java
@@ -1,7 +1,8 @@
 package com.playhive.batch.schedule;
 
+import com.playhive.batch.crawler.game.GameCrawler;
 import java.util.Date;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -13,55 +14,63 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
-
 @Component
 @RequiredArgsConstructor
 public class Scheduler {
-	private final Job newsCrawlJob;
-	private final Job matchCrawlJob;
-	private final Job gameCrawlJob;
-	private final JobLauncher jobLauncher;
+    private final Job newsCrawlJob;
+    private final Job matchCrawlJob;
+    //    private final Job gameCrawlJob;
+    private final GameCrawler gameCrawler;
+    private final JobLauncher jobLauncher;
 
-	@Scheduled(cron = "0 0 */2 * * *") // 매일 2시간 마다 실행
-	public void newsCrawlJob() throws
-		JobInstanceAlreadyCompleteException,
-		JobExecutionAlreadyRunningException,
-		JobParametersInvalidException,
-		JobRestartException {
+    @Scheduled(cron = "0 0 */2 * * *") // 매일 2시간 마다 실행
+    public void newsCrawlJob() throws
+            JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException,
+            JobParametersInvalidException,
+            JobRestartException {
 
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addDate("date", new Date())
-			.addLong("time", System.currentTimeMillis())
-			.toJobParameters();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addDate("date", new Date())
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
 
-		this.jobLauncher.run(newsCrawlJob, jobParameters);
-	}
+        this.jobLauncher.run(newsCrawlJob, jobParameters);
+    }
 
-	@Scheduled(cron = "0 0 7 * * *") // 매일 오전 7시 0분 0초에 실행
-	public void matchCrawlJob() throws
-		JobInstanceAlreadyCompleteException,
-		JobExecutionAlreadyRunningException,
-		JobParametersInvalidException,
-		JobRestartException {
+    @Scheduled(cron = "0 0 7 * * *") // 매일 오전 7시 0분 0초에 실행
+    public void matchCrawlJob() throws
+            JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException,
+            JobParametersInvalidException,
+            JobRestartException {
 
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addDate("date", new Date())
-			.addLong("time", System.currentTimeMillis())
-			.toJobParameters();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addDate("date", new Date())
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
 
-		this.jobLauncher.run(matchCrawlJob, jobParameters);
-	}
+        this.jobLauncher.run(matchCrawlJob, jobParameters);
+    }
 
-	@Scheduled(cron = "0 0 23 * * *") // 매일 오후 11에 다음날에 노출될 게임 정보 크롤링 실행
-	public void gameEventCrawl() throws JobInstanceAlreadyCompleteException,
-		JobExecutionAlreadyRunningException,
-		JobParametersInvalidException, JobRestartException {
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addDate("date", new Date())
-			.addLong("time", System.currentTimeMillis())
-			.toJobParameters();
+//    게임 이벤트 크롤링 배치 코드
+//    @Scheduled(cron = "0 0 23 * * *") // 매일 오후 11에 다음날에 노출될 게임 정보 크롤링 실행
+//    public void gameEventCrawl() throws JobInstanceAlreadyCompleteException,
+//            JobExecutionAlreadyRunningException,
+//            JobParametersInvalidException, JobRestartException {
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addDate("date", new Date())
+//                .addLong("time", System.currentTimeMillis())
+//                .toJobParameters();
+//
+//        this.jobLauncher.run(gameCrawlJob, jobParameters);
+//    }
 
-		this.jobLauncher.run(gameCrawlJob, jobParameters);
-	}
+    /**
+     * 게임 이벤트 크롤링은 스케줄러만 사용
+     */
+    @Scheduled(cron = "0 0 23 * * *") // 매일 오후 11에 다음날에 노출될 게임 정보 크롤링 실행
+    public void gameEventCrawl() {
+        gameCrawler.crawl();
+    }
 }
