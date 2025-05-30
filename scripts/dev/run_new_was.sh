8264cf7327035ad866368e0fdb1d13d235cd9069
송지영 <thdwldud8@gmail.com>
2025-05-30 23:34:55 +0900

    chore: 배포 시 중복 실행되는 배치 프로세스 종료 처리 추가

--- scripts/dev/run_new_was.sh
+++ scripts/dev/run_new_was.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app
-
 TARGET_PORT=8080
-
-TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
-
-echo "> Kill WAS running at ${TARGET_PORT}."
-sudo kill ${TARGET_PID}
-
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME에 실행 권한 추가"
-chmod +x $JAR_NAME
-
 LOG_FILE="$REPOSITORY/nohup.out"
 
-echo "> 로그 파일이 존재하지 않으면 생성하고, 존재하면 내용을 유지 (쓰기 권한 확인)"
+echo "> 🔍 기존 batch jar 프로세스 종료 시도..."
+PIDS=$(ps -ef | grep 'batch-0.0.1-SNAPSHOT.jar' | grep -v grep | awk '{print $2}')
+
+if [ -n "$PIDS" ]; then
+  echo "> 🔴 종료할 PID 목록: $PIDS"
+  kill -9 $PIDS
+else
+  echo "> ✅ 실행 중인 배치 프로세스 없음"
+fi
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+echo "> 🔍 JAR 파일: $JAR_NAME"
+
+echo "> 🔧 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> 📄 로그 파일 확인 및 생성"
 touch $LOG_FILE
 
-echo "> $JAR_NAME 실행"
-nohup java -jar \
-          -Dserver.port=${TARGET_PORT} \
+echo "> 🚀 배치 실행 시작"
+nohup java -Duser.timezone=Asia/Seoul -jar \
           $JAR_NAME > $LOG_FILE 2>&1 &
 
-echo "> Now new WAS runs at ${TARGET_PORT}."
+echo "> ✅ 배치가 정상 실행되었습니다."
 exit 0
