e1917d66ce29b12ed0b4ab698f2861096832852f
Kyoungwoong <kyy980708@gmail.com>
2025-07-03 19:09:19 +0900

    fix: 의미없는 메소드 삭제

--- src/main/java/com/playhive/batch/news/repository/NewsSearchRepository.java
+++ src/main/java/com/playhive/batch/news/repository/NewsSearchRepository.java
@@ -6,9 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface NewsSearchRepository extends ElasticsearchRepository<NewsDocument, String> {
-
-    // 기본 쿼리: 제목 또는 내용에 키워드 포함
-    Page<NewsDocument> findByTitleContainingOrContentContaining(
-            String title, String content, Pageable pageable
-    );
+    
 }
