c13951e74fdbfc82687ca30f57faf9d35ac3d779
송지영 <thdwldud8@gmail.com>
2025-07-21 15:36:36 +0900

    chore: kbo리그 일정 URL 수정

--- src/main/java/com/playhive/batch/crawler/match/baseball/KboMatchCrawler.java
+++ src/main/java/com/playhive/batch/crawler/match/baseball/KboMatchCrawler.java
@@ -29,7 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class KboMatchCrawler implements MatchCrawler {
 
-    private static final String URL = "https://m.sports.naver.com/kbaseball/schedule/index?date=";
+    private static final String URL = "https://m.sports.naver.com/kbaseball/schedule/index?category=kbo&date=";
 
     private static final String LEAGUE_CLASS = "ScheduleAllType_match_list_group__1nFDy";
     private static final String LEAGUE_NAME = "ScheduleAllType_title___Qfd4";
